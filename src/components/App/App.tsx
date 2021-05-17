562eb080cf35aeb3017921a2b8f3252bbe5043aa
Samaila Bala <samailabalap@gmail.com>
2021-05-17 21:03:45 +0100

    chore: setup project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>takada.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
