c78512a544b4bce15658a0b00915dcd6578554fe
Samaila Bala <samailabalap@gmail.com>
2021-05-18 16:06:01 +0100

    feat: add date to notes

--- src/components/Notepad/Notepad.tsx
+++ src/components/Notepad/Notepad.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import dayjs from 'dayjs';
 import './Notepad.scss';
 
-const Notepad = () => {
+type NoteProps = {
+  date: string;
+  note: string;
+};
+const Notepad = ({ date, note }: NoteProps) => {
+  const [text, setText] = React.useState('');
+
+  // check if date is current
+  const checkNoteStatus = (noteDate: string): boolean => {
+    if (dayjs().isSame(dayjs(date, 'DD-MM-YYYY'), 'day')) {
+      return false;
+    }
+    return true;
+  };
+
+  console.log(checkNoteStatus(date));
+  React.useEffect(() => {
+    setText(note || '');
+  }, [note]);
+
   return (
     <div className="notepad">
       <div className="top"></div>
-      <div className="paper" contentEditable="true">
-        Click here to start writing.
+      <div className="date">
+        {dayjs(date, 'DD-MM-YYYY').format('ddd DD, MMM YYYY')}
       </div>
+      <textarea
+        className="paper"
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        disabled={checkNoteStatus(date)}
+        placeholder="Click here to start writing."
+      />
     </div>
   );
 };
--- src/components/Notes/Notes.tsx
+++ src/components/Notes/Notes.tsx
@@ -2,11 +2,32 @@ import React from 'react';
 import Notepad from '../Notepad/Notepad';
 import './Notes.scss';
 
+const data = [
+  {
+    date: '2021-05-18',
+    note: 'Praesent blandit laoreet nibh. Nunc nonummy metus. Vestibulum fringilla pede sit amet augue.',
+  },
+  {
+    date: '2021-05-17',
+    note: 'Praesent blandit laoreet nibh. Nunc nonummy metus. Vestibulum fringilla pede sit amet augue.',
+  },
+  {
+    date: '2021-05-16',
+    note: 'Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.',
+  },
+  {
+    date: '2021-05-15',
+    note: 'Etiam feugiat lorem non metus.',
+  },
+];
+
 const Notes = () => {
   return (
     <div className="notes">
-      <Notepad />
-      <Notepad />
+      {data &&
+        data.map((item) => (
+          <Notepad key={item.date} date={item.date} note={item.note} />
+        ))}
     </div>
   );
 };
